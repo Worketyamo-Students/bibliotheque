8cd451a5930462ae4d3fe3c12aa5ecab58d876fe
haruna-rashid-yakubu <haruna.rashid.yakubu@gmail.com>
2025-07-04 19:59:55 +0200

    feat: Implement book and loan management
    This commit introduces the functionality for managing books and loans, including controllers, routes, and database interactions using Prisma. It allows for creating, reading, updating, and deleting books and loans.

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import morgan from "morgan";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import user from "./routes/user.routes.ts";
+import book from "./routes/books.routes.ts";
+import loan from "./routes/loan.routes.ts";
 configDotenv();
 const app = express();
 app.use(morgan("combined"));
@@ -12,6 +14,9 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/users", user);
+app.use("/books", book);
+app.use("/loans", loan);
+
 app.listen(port, () => {
   console.log("le serveur tourne sur http://localhost:" + port);
 });
